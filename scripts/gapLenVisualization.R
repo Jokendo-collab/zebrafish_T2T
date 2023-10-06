15d0a69d57e43c304ffa3d62889605eababeebe4
Javan Okendo <javanokendo@gmail.com>
2023-10-06 11:27:20 -0400

    Update gapLenVisualization.R

--- scripts/gapLenVisualization.R
+++ scripts/gapLenVisualization.R
@@ -43,3 +43,45 @@ ggplot(gap_data, aes(x = Chromosome, y = Gap_Length)) +
 # Optional: Export the gap data or plots as needed
 # write.csv(gap_data, "gap_data.csv")
 ggsave("gap_length_distribution.pdf", height = 6, width = 8)
+
+
+
+library(karyoploteR)
+
+# Define the paths to your FASTA files (genome assemblies) and BED file (syntenic regions)
+fasta_file1 <- "ref_scaffold.fasta"
+fasta_file2 <- "genome2.fasta"
+bed_file <- "syntenic_regions.bed"
+
+# Load the genome assemblies from the FASTA files
+genome1 <- getGenome(fasta_file1)
+genome2 <- getGenome(fasta_file2)
+
+# Create a plot with two ideograms (for two genomes)
+kp <- plotKaryotype(list(genome1, genome2), ideogram.plotter = NULL)
+
+# Add syntenic regions from the BED file
+addBed(kp, bed_file, col = "blue", lwd = 2)
+
+# Customize the plot (optional)
+kp <- set.karyoplot(kp, chr.padding = 0.5)
+
+# Save the plot as an image file (e.g., PNG)
+output_file <- "synteny_plot.png"
+savePlot(kp, output_file)
+
+
+kp <- plotKaryotype(genome="danRer11", plot.type=2, chromosomes=c("chr1", "chr2", "chr3","chr4","chr5","chr6","chr7",
+                                                              "chr8","chr9","chr10","chr11","chr12","chr13","chr14",
+                                                              "chr15","chr16","chr17","chr18","chr19","chr20","chr21"
+                                                              ,"chr22","chr23","chr24","chr25","chrM"))
+
+
+
+
+
+
+
+
+
+
